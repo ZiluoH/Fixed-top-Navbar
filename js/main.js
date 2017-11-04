fbea1c0f23a3bea9efdab84a7ec86deafa0c7722
ZiluoH <ziluoh@gmail.com>
2017-11-04 15:55:35 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var obj = document.getElementById("wrap");
+    var ot = obj.offsetTop;
+    document.onscroll = function () {
+        var st = document.body.scrollTop || document.documentElement.scrollTop;
+        obj.setAttribute("data-fixed",st >= ot?"fixed":"")}
